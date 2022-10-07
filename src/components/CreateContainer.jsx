52651e67933e69485358397fc5a8a9623e24c3e4
osama355 <muhammadosama3556@gmail.com>
2022-10-08 00:02:18 +0500

    update 4

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,11 +1,88 @@
-import React from 'react'
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { MdFastfood } from "react-icons/md";
+import { categories } from "../utils/data";
 
 function CreateContainer() {
+  const [itemData, setItemData] = useState({
+    title: "",
+    calories: "",
+    price: "",
+    category: null,
+    imageAsset: null,
+    fields: false,
+    alertStatus: "danger",
+    msg: null,
+    isLoading: false,
+  });
+
+  let name, value;
+  const getItemData = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+    setItemData({
+      ...itemData,
+      [name]: value,
+    });
+  };
+
   return (
-    <div className='w-full min-h-screen flex items-center justify-center'>
-      <div className='w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center'></div>
+    <div className="w-full min-h-screen flex items-center justify-center">
+      <div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center gap-4">
+        {itemData.fields && (
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className={`w-full p-2 rounded-lg text-center text-lg font-semibold ${
+              itemData.alertStatus === "danger"
+                ? "bg-red-400 text-red-800"
+                : "bg-emerald-400 text-emerald-800"
+            }`}
+          >
+            {itemData.msg}
+          </motion.p>
+        )}
+
+        <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
+          <MdFastfood className="text-xl text-gray-700" />
+          <input
+            type="text"
+            name="title"
+            required
+            value={itemData.title}
+            onChange={getItemData}
+            placeholder="Give me a title..."
+            className="w-full h-full text-lg bg-transparent font-semibold outline-none border-none placeholder:text-gray-400 text-textColor"
+          />
+        </div>
+
+        <div className="w-full">
+          <select
+            name="category"
+            onChange={getItemData}
+            className="outline-none w-full text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer"
+          >
+            <option value="other" className="bg-white">
+              Select Category
+            </option>
+            {categories &&
+              categories.map((item) => (
+                <option
+                  key={item.id}
+                  className="w-full text-base border-0 outline-none capitalize bg-white text-headingColor"
+                  value={item.urlParamName}
+                >
+                  {item.name}
+                </option>
+              ))}
+          </select>
+        </div>
+
+        <div className="group flex items-center justify-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-300 cursor-pointer rounded-lg"></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CreateContainer
+export default CreateContainer;
